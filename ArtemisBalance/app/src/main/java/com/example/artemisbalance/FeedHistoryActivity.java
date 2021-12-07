f4287028be30db9f5ea6827234f7219a96cbf261
Faruk <fceylan92@gmail.com>
2021-12-07 14:57:17 +0300

    Feed History Screen is added

---
+++ ArtemisBalance/app/src/main/java/com/example/artemisbalance/FeedHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.artemisbalance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeedHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_history);
+    }
+}
