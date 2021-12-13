19f4988ba9c9681fa85612a63c856eb02ca30876
Faruk <fceylan92@gmail.com>
2021-12-13 19:19:43 +0300

    Adding new features

--- MapApp/app/src/main/java/com/example/mapapp/MainActivity.java
+++ MapApp/app/src/main/java/com/example/mapapp/MainActivity.java
@@ -7,24 +7,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.mapapp.connector.Register;
+
 public class MainActivity extends AppCompatActivity {
 
 
     Button login;
+    Button register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         login = findViewById(R.id.button2);
-        login.setOnClickListener(new View.OnClickListener() {
-                                     @Override
-                                     public void onClick(View v) {
-                                         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                                         startActivity(intent);
 
-                                     }
-                                 }
-        );
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        register = findViewById(R.id.button4);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- MapApp/app/src/main/java/com/example/mapapp/MenuActivity.java
+++ MapApp/app/src/main/java/com/example/mapapp/MenuActivity.java
@@ -2,13 +2,46 @@ package com.example.mapapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
+    Button profile;
+    Button feedHistory;
+    Button contactUs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        profile = findViewById(R.id.button6);
+        feedHistory = findViewById(R.id.button7);
+        contactUs = findViewById(R.id.button8);
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        feedHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, FeedHistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        contactUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, ContactActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
