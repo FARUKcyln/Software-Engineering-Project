50f0b300edb2cc61894ac1030a7eb18b98ca1f29
Faruk <fceylan92@gmail.com>
2022-03-02 13:29:32 +0300

    Conflicts are fixed

--- MapApp/app/src/main/java/com/example/mapapp/LoginActivity.java
+++ MapApp/app/src/main/java/com/example/mapapp/LoginActivity.java
@@ -41,8 +41,11 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 <<<<<<< HEAD
+<<<<<<< HEAD
                 Login l = new Login(String.valueOf(e_mail.getText()), String.valueOf(password.getText()));
 =======
+=======
+>>>>>>> parent of 6279e41 (Map has been edited.)
                 /*Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
                 startActivity(intent);*/
                 Login l = new Login(String.valueOf(e_mail.getText()), String.valueOf(password.getText()));
